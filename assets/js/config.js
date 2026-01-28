f66042b081588f05c4a7180e454d98252d040c63
AlexVNX <129174676+AlexVNX@users.noreply.github.com>
2026-01-28 12:12:01 +0100

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 // assets/js/config.js
 export const SUPABASE = {
   URL: "https://xjszetgxhrrdqtxgmfvo.supabase.co",
-  ANON_KEY: "PEGA_AQUI_TU_ANON_PUBLIC_KEY",
+  ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhqc3pldGd4aHJyZHF0eGdtZnZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1OTY0MDksImV4cCI6MjA4NTE3MjQwOX0.CTH-0yCG-4g76uRJcbI4wB9MJVDUnap6fl738Kgc-Yw",
   FUNCTIONS_URL: "https://xjszetgxhrrdqtxgmfvo.functions.supabase.co"
 };

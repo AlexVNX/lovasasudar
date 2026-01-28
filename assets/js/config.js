8d6d63561093cf74718a98ad4bdea8c1a7f52ba2
AlexVNX <129174676+AlexVNX@users.noreply.github.com>
2026-01-28 12:00:49 +0100

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+// assets/js/config.js
+export const SUPABASE = {
+  URL: "https://xjszetgxhrrdqtxgmfvo.supabase.co",
+  ANON_KEY: "PEGA_AQUI_TU_ANON_PUBLIC_KEY",
+  FUNCTIONS_URL: "https://xjszetgxhrrdqtxgmfvo.functions.supabase.co"
+};
